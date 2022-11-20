a1a56a4a313da52a4f16866c14d36de2ed413c95
rehan18624 <95132887+rehan18624@users.noreply.github.com>
2022-11-20 11:19:31 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,9 +15,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = "12842782"
 API_HASH = "b9af9690e4e36df1f8d356e1a8e376a9"
 BOT_TOKEN = "5961413879:AAFvwFP5-STmvAo2J34wZo19J8JmAmISB48"
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+SESSION = "BQBlUUJNRToWtoyTUQTJ3pg11pjh-AN0FSaa6-5DkfajnecLvqPvd3-fD8VAhdYtc0znW1wnwsIoi58otyK-VYxUsKa_rfLUICdoGnTlbawyeQmyYpy43hBZypuBJR3O5b5ZpabkrIdIOgZkv_GDnaTFeAH6q-7IM7yFJXWf2RS8KQdupbHFVyCPExfpAiSg7Ap3-Sw3HlcHGWlEjRgY0ujOHC64ocbiiaX0iDtQsVoBt-o-n9DFUjQvObjLHch4neICFtoUI_rS6_cXclUJAOBPjHOUcy-fMl8e8YaNpqYBPiZt4s8qa1Kgw5MMzipfRXsLeWhrsZFLxQZA2rymBghEAAAAAVHqXYkA"
+FORCESUB = "restrictedsaverwarnisx"
+AUTH = "5669281161"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
